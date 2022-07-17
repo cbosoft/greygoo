ba2058d90dbc70f6d97ec6cc411189574eea9ad5
Christopher Boyle <chrismjboyle@gmail.com>
2022-07-17 21:03:24 +0100

    Fix format issue in time format function

--- src/fmt_t.rs
+++ src/fmt_t.rs
@@ -5,11 +5,11 @@ pub fn fmt_t(s: i64) -> String {
     }
     else if s > 86400 {
         let d = (s as f64) / 86400f64;
-        format!("{:.1}w", d)
+        format!("{:.1}d", d)
     }
     else if s > 3600 {
         let h = (s as f64) / 3600f64;
-        format!("{:.1}w", h)
+        format!("{:.1}h", h)
     }
     else if s > 60 {
         let m = (s as f64) / 60f64;
